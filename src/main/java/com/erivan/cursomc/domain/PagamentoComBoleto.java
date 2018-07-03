6e7e93d6129e41d1daa738af87d72475909adb1a
Erivan <erivann47@gmail.com>
2018-07-03 18:42:07 -0300

    Uso do JsonInore nas classes 'Model'

--- src/main/java/com/erivan/cursomc/domain/PagamentoComBoleto.java
+++ src/main/java/com/erivan/cursomc/domain/PagamentoComBoleto.java
@@ -3,8 +3,6 @@ package com.erivan.cursomc.domain;
 import java.util.Date;
 
 import javax.persistence.Entity;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 
 import com.erivan.cursomc.domain.enums.EstadoPagamento;
 import com.fasterxml.jackson.annotation.JsonFormat;
