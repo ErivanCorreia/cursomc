0daa6dd07989492a553fc38bb0fe787c9edb1dcd
Erivan <erivann47@gmail.com>
2018-07-05 15:56:30 -0300

    Atualizando um Categoria com PUT + alguns ajustes padões no código

--- src/main/java/com/erivan/cursomc/resources/CategoriaResources.java
+++ src/main/java/com/erivan/cursomc/resources/CategoriaResources.java
@@ -24,9 +24,9 @@ public class CategoriaResources {
 	private CategoriaService service;
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id) {
+	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
 		
-		Categoria obj = service.buscar(id);
+		Categoria obj = service.find(id);
 		
 		return ResponseEntity.ok().body(obj);
 	}
@@ -40,5 +40,12 @@ public class CategoriaResources {
 			.path("/{id}").buildAndExpand(obj.getId()).toUri();
 		
 		return ResponseEntity.created(uri).build();//ResponseEntity.ok()... também iria servir
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id){
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();//noCorrent() por que quero retornar um conteúdo vazio, poderia também ser ok()!
 	}
 }
