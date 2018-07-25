3ef7fdcf59275f62f3c5ae5c195552e70b465b68
Erivan <erivann47@gmail.com>
2018-07-25 09:51:36 -0300

    Inserindo um Pedido

--- src/main/java/com/erivan/cursomc/domain/PagamentoComCartao.java
+++ src/main/java/com/erivan/cursomc/domain/PagamentoComCartao.java
@@ -3,8 +3,10 @@ package com.erivan.cursomc.domain;
 import javax.persistence.Entity;
 
 import com.erivan.cursomc.domain.enums.EstadoPagamento;
+import com.fasterxml.jackson.annotation.JsonTypeName;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento{
 	private static final long serialVersionUID = 1L;
 	
---
+++ src/main/java/com/erivan/cursomc/services/BoletoService.java
@@ -0,0 +1,21 @@
+package com.erivan.cursomc.services;
+
+
+import java.util.Calendar;
+import java.util.Date;
+
+import org.springframework.stereotype.Service;
+
+import com.erivan.cursomc.domain.PagamentoComBoleto;
+
+@Service
+public class BoletoService {
+
+	public void preencherPagamentoComBoleto(PagamentoComBoleto pgto, Date instante) {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(instante);
+		cal.add(Calendar.DAY_OF_MONTH, 7);
+		pgto.setDataVencimento(cal.getTime());
+	}
+	
+}
