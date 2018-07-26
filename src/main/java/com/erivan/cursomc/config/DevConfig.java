37a65108422af9aa90f520d13b74d8f22f1855e8
Erivan <erivann47@gmail.com>
2018-07-26 09:32:27 -0300

    Criando profile de desenvolvimento

---
+++ src/main/java/com/erivan/cursomc/config/DevConfig.java
@@ -0,0 +1,33 @@
+package com.erivan.cursomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.erivan.cursomc.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	
+	@Autowired
+	public DBService dbService; 
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	public String strategy;
+	
+	@Bean
+	public boolean instantianteDataBase() throws ParseException {
+		
+		if(!"create".equals(strategy)) {
+			return false;
+		}
+		
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+}
