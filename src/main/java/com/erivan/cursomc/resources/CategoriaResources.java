270ed9615c5a3c96ccdd78ea3784972103e4aea6
Erivan <erivann47@gmail.com>
2018-09-18 02:57:35 -0300

    Autorizando endpoints para perfis específicos

--- src/main/java/com/erivan/cursomc/resources/CategoriaResources.java
+++ src/main/java/com/erivan/cursomc/resources/CategoriaResources.java
@@ -10,6 +10,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,6 +39,7 @@ public class CategoriaResources {
 		return ResponseEntity.ok().body(obj);
 	}
 	
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(method=RequestMethod.POST)
 	public ResponseEntity<Void> insert(@Valid @RequestBody CategoriaDTO objDTO){
 		
@@ -50,6 +52,7 @@ public class CategoriaResources {
 		return ResponseEntity.created(uri).build();//ResponseEntity.ok()... também iria servir
 	}
 	
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
 	public ResponseEntity<Void> update(@Valid @RequestBody CategoriaDTO objDTO, @PathVariable Integer id){
 		
@@ -60,6 +63,7 @@ public class CategoriaResources {
 		return ResponseEntity.noContent().build();//noCorrent() por que quero retornar um conteúdo vazio, poderia também ser ok()!
 	}
 	
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable Integer id){
 		
