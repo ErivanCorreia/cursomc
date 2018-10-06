c84c07c82c63be5d542caa0d91e20337a992fc3f
Erivan <erivann47@gmail.com>
2018-10-06 20:54:15 -0300

    esqueci a senha

--- src/main/java/com/erivan/cursomc/services/AbstractEmailService.java
+++ src/main/java/com/erivan/cursomc/services/AbstractEmailService.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 
+import com.erivan.cursomc.domain.Cliente;
 import com.erivan.cursomc.domain.Pedido;
 
 public abstract class AbstractEmailService implements EmailService {
@@ -37,6 +38,30 @@ public abstract class AbstractEmailService implements EmailService {
 		return sm;
 	}
 	
+	@Override
+	public void sendNewPasswordEmail(Cliente cliente, String newPass) {
+		SimpleMailMessage sm = pepareNewPassword(cliente, newPass);
+		sendEmail(sm);
+	}
+
+
+	protected SimpleMailMessage pepareNewPassword(Cliente cliente, String newPass) {
+		
+		SimpleMailMessage sm = new SimpleMailMessage();
+		//recebe o email
+		sm.setTo(cliente.getEmail());
+		
+		//envia a o email
+		sm.setFrom(sender);
+		
+		//Título do email
+		sm.setSubject("Auteração de senha");
+		sm.setSentDate(new Date(System.currentTimeMillis()));
+		
+		//Corpo do email
+		sm.setText("Nova senha: "+newPass);
+		return sm;
+	}
 	
 	
 }
