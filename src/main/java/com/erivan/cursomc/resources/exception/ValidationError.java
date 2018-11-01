fdf52a8f5e77583625b1778ddee9109f61163c2e
Erivan <erivann47@gmail.com>
2018-11-01 11:49:37 -0300

    Padronização de Exceções

--- src/main/java/com/erivan/cursomc/resources/exception/ValidationError.java
+++ src/main/java/com/erivan/cursomc/resources/exception/ValidationError.java
@@ -7,10 +7,11 @@ public class ValidationError extends StandardError {
 	private static final long serialVersionUID = 1L;
 
 	public List<FieldMessage> erros = new ArrayList<>();
-	public ValidationError(Integer status, String msg, Long timeStamp) {
-		super(status, msg, timeStamp);
-		
+	
+	public ValidationError(Long timeStamp, Integer status, String error, String message, String path) {
+		super(timeStamp, status, error, message, path);
 	}
+	
 	public List<FieldMessage> getErros() {
 		return erros;
 	}
